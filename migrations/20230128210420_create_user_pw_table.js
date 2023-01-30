fd2bde78e8edacad4b1c2adc5123bcf3dd49f66e
Kevin Tran <kevtran@hotmail.com>
2023-01-29 16:01:32 -0800

    Github User login done

---
+++ migrations/20230128210420_create_user_pw_table.js
@@ -0,0 +1,60 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+    return knex.schema
+        .createTable("users", (table) => {
+            table.increments("id").primary();
+            table.integer("github_id");
+            table.string("username").notNullable();
+            table.string("name");
+            table.string("password");
+            table.timestamp("updated_at").defaultTo(knex.fn.now());
+        })
+        .createTable("weeks", (table) => {
+            table.integer("user_id").unsigned().notNullable();
+            table.integer("week_id").unsigned().notNullable();
+            table
+                .foreign("user_id")
+                .references("id")
+                .inTable("users")
+                .onUpdate("CASCADE")
+                .onDelete("CASCADE");
+            table.primary(["week_id", "user_id"]);
+            table.timestamp("updated_at").defaultTo(knex.fn.now());
+        })
+        .createTable("exercises", (table) => {
+            table.increments("id").primary();
+            table.string("day", 9).notNullable();
+            table.string("exercise").notNullable();
+            table.integer("sets").unsigned().notNullable();
+            table.integer("reps").unsigned().notNullable();
+            table.integer("weight").unsigned().notNullable();
+        })
+        .createTable("week_exercise", (table) => {
+            table.increments("id").primary();
+            table.integer("user_id").unsigned().notNullable();
+            table.integer("week_id").unsigned().notNullable();
+            table.integer("exercise_id").unsigned().notNullable();
+            table.timestamp("updated_at").defaultTo(knex.fn.now());
+
+            table.foreign("user_id").references("user_id").inTable("weeks");
+
+            table.foreign("week_id").references("week_id").inTable("weeks");
+
+            table.foreign("exercise_id").references("id").inTable("exercises");
+        });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+    return knex.schema
+        .dropTable("users")
+        .dropTable("weeks")
+        .dropTable("exercises")
+        .dropTable("week_exercise");
+};
