230fd03e0b733fc2d97d884572f832b32d840095
Kevin Tran <kevtran@hotmail.com>
2023-02-20 12:13:46 -0800

    Test8

--- index.js
+++ index.js
@@ -65,7 +65,6 @@ app.use((req, res, next) => {
     let authRouteCheck = req.url.substring(0, 5);
 
     if (req.url === "/signup" || authRouteCheck === "/auth") {
-        console.log(req.user, "Token Check Level");
         next();
     } else {
         const token = getToken(req);
